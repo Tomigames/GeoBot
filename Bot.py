798e2ba0546d3273a5ac3790d4c6d45d962a8b98
Fellox <computeroj09@gmail.com>
2019-05-06 21:08:42 -0600

    Se ha actualizado el codigo del repositorio con el local.

--- Bot.py
+++ Bot.py
@@ -7,24 +7,32 @@ bot = commands.Bot(command_prefix='g!')
 TOKEN = 'NTQ4MjgzOTU1MDc0MzAxOTcy.XMjQhg._scWw4Rxz2TvKLwfk7_1bMEUfqg'
 
 @bot.event
-def on_ready():
-    await bot.remove_command('help')
+async def on_ready():
     channel = bot.get_channel(547600921035669516)
     print('Corriendo!')
-    await bot.channel.send(channel, '¡Listo para rodar!')
+    await channel.send('¡Listo para rodar!')
 
 @bot.command()
 async def ping(ctx):
     await ctx.send(f'Pong! `{round(bot.latency * 1000)}ms`')
 
 @bot.command()
-async def masterhelp():
-    embed = discord.Embed(title='Lista de Comandos', description='Lista de Comandos para GeoBot', colour=discord.Colour.blue)
-    embed.set_footer(text='Hecho por [Fellox]')
-    embed.add_field(name='g!ping', value='Devuelve un comando con `Pong!` y la latencia del bot.')
-    await bot.say(embed=embed)
+async def ayuda(ctx):
+    await ctx.send('''
+    ===============================
+    **_Lista de Comandos_**
+
+    `g!ping`
+    Envia un mensaje con la latencia del bot.
+
+    `g!ayuda`
+    Envia este mensaje
+    ===============================
+    ''')
 
 @bot.command()
-async def fellox():
-    await bot.say('A su ordenes!')
+async def shutdown(ctx):
+    await ctx.send('Shuting down internal server.')
+    exit()
+
 bot.run(TOKEN)
