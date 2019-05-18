98872b58bc9914d834284a781592a4c7206a213c
Fellox <computeroj09@gmail.com>
2019-05-18 11:32:25 -0600

    Se agrego el token del bot (otra vez)

--- Bot.py
+++ Bot.py
@@ -5,8 +5,7 @@ from discord.ext import commands
 import random
 
 bot = commands.Bot(command_prefix='g!')
-TOKEN = 'e.e'
-
+TOKEN = 'NTQ4MjgzOTU1MDc0MzAxOTcy.XMjQhg._scWw4Rxz2TvKLwfk7_1bMEUfqg'
 @bot.event
 async def on_ready():
     channel = bot.get_channel(547600921035669516)
